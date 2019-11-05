86924259b06f103d3864d0f4cf45b571c2ab0c16
frizzonelli <fabrizio.rizzonelli@gmail.com>
2019-11-05 12:20:00 +0100

    fixed typings

--- src/typings/react-native-parallax-header.d.ts
+++ src/typings/react-native-parallax-header.d.ts
@@ -1,10 +1,21 @@
 
 declare module 'react-native-parallax-header' {
     import React from 'react';
-    import { NativeMethodsMixinStatic, ScrollViewProperties } from "react-native";
+    import { NativeMethodsMixinStatic, ScrollViewProperties, StyleProp, ViewStyle } from "react-native";
 
     export interface ReactNativeParallaxHeaderProps extends ScrollViewProperties {
-        
+        renderContent: () => JSX.Element;
+        backgroundImage?: string;
+        backgroundImageScale?: number;
+        navbarColor?: string;
+        headerMinHeight?: number;
+        headerMaxHeight?: number;
+        alwaysShowTitle?: boolean;
+        extraScrollHeight?: number;
+        renderNavBar?: () => JSX.Element;
+        title?: JSX.Element | string;
+        contentContainerStyle?: StyleProp<ViewStyle>;
+        // renderContent = {() => (
     }
 
     export interface ReactNativeParallaxHeaderStatic extends NativeMethodsMixinStatic, React.ComponentClass<ReactNativeParallaxHeaderProps> {
